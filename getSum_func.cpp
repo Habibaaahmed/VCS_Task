2165d23e0e0eded50c2df60b6ed8e68fa1fd2549
Habiba ahmed alaa eldin <pop82000@gmail.com>
2023-11-03 02:50:42 +0200

    gerAvg

--- getSum_func.cpp
+++ getSum_func.cpp
@@ -1,19 +1,13 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-#include <iostream>
-#include <vector>
-
-int getSum(const std::vector<int>& numbers) {
-    int sum = 0;
-    for (int num : numbers) {
-        sum += num;
-    }
-    return sum;
+double getAverage(const std::vector<int>& numbers) {
+    int sum = getSum(numbers);
+    return static_cast<double>(sum) / numbers.size();
 }
 
 int main() {
-    std::vector<int> numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int sum = getSum(numbers);
+    double average = getAverage(numbers);
     std::cout << "Sum of numbers: " << sum << std::endl;
+    std::cout << "Average of numbers: " << average << std::endl;
     return 0;
 }
