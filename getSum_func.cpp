11414f394ffa59cd1d3a2459a6e152f6d9a01453
Habiba ahmed alaa eldin <pop82000@gmail.com>
2023-11-03 02:59:00 +0200

    gitSum_fun

--- getSum_func.cpp
+++ getSum_func.cpp
@@ -1,13 +1,17 @@
-double getAverage(const std::vector<int>& numbers) {
-    int sum = getSum(numbers);
-    return static_cast<double>(sum) / numbers.size();
+#include <iostream>
+#include <vector>
+
+int getSum(const std::vector<int>& numbers) {
+    int sum = 0;
+    for (int num : numbers) {
+        sum += num;
+    }
+    return sum;
 }
 
 int main() {
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int sum = getSum(numbers);
-    double average = getAverage(numbers);
     std::cout << "Sum of numbers: " << sum << std::endl;
-    std::cout << "Average of numbers: " << average << std::endl;
     return 0;
 }
